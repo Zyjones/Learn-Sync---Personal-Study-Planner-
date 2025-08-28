6739f20461eb10a2f92d1a545b7f994ddb2716d6
Zakiyyah Jones <zjones.if25@MacBookAir-TKH0006.local>
2025-08-28 17:06:52 -0400

    Added source folder with python scripts

---
+++ source/main.py
@@ -0,0 +1,11 @@
+from source.utils import squarer
+
+def main():
+    #Import data 
+    # Clean the data
+    # Embeddings
+    # Creating Vector Store 
+
+if __name__ == "__main__":
+    main()
+    #call the agents 
